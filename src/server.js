d6382ac47c827f9e41f2f0c273e0a674610edce3
Yan Gameiro <yan.gameiro@gmail.com>
2019-10-05 07:24:12 -0300

    making spots return the thumbnail's URL

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
@@ -15,6 +16,9 @@ mongoose.connect(process.env.DB_STRING_CONNECTION, {
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(
+    path.resolve( __dirname, '..', 'uploads')
+));
 app.use(routes);
 
 console.log('started at :3333');

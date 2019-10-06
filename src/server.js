1b72269677e487661b0fa45aa933c41dd6995253
Yan Gameiro <yan.gameiro@gmail.com>
2019-10-06 18:57:01 -0300

    starting to use socketio

--- src/server.js
+++ src/server.js
@@ -3,16 +3,32 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
+const socketio = require('socket.io');
+const http = require('http');
 
 const routes = require('./routes');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 mongoose.connect(process.env.DB_STRING_CONNECTION, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
-''
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user_id } = socket.handshake.query;
+    connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+    return next();
+})
 
 app.use(cors());
 app.use(express.json());
@@ -23,4 +39,4 @@ app.use(routes);
 
 console.log('started at :3333');
 
-app.listen(3333);
+server.listen(3333);

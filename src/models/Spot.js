2cd2a51088eba5dcae1ba84d32e1041d97c59110
Yan Gameiro <yan.gameiro@gmail.com>
2020-05-29 09:29:02 -0300

    change query of Spot index

--- src/models/Spot.js
+++ src/models/Spot.js
@@ -3,44 +3,44 @@ const db = require('../database/database');
 const TABLE_NAME = 'spots';
 
 module.exports = {
-    async insertSpot (spotData) {
+    async insertSpot(spotData) {
 
-        if(spotData.price == '') {
+        if (spotData.price == '') {
             delete spotData.price;
         }
 
         const result = await db.insert(TABLE_NAME, spotData);
-        
-        return {id: result.insertId, spotData};
+
+        return { id: result.insertId, spotData };
     },
-    async index () {
-        let sql = 'SELECT id, techs, owner_user_id, thumbnail_image_id, company_name, price FROM spots'; 
+    async index() {
+        let sql = 'select s.id, s.techs, s.owner_user_id, f.`file`, s.company_name, s.price from spots as s join files as f on s.thumbnail_image_id = f.id';
         return await db.query(sql, []);
     },
-    async findByTech (tech) {
+    async findByTech(tech) {
 
-        let sql = 'select s.id, s.techs, s.owner_user_id, f.`file`, s.company_name, s.price from spots as s join files as f on s.thumbnail_image_id = f.id where s.techs LIKE \'%' + tech + '%\';'; 
+        let sql = 'select s.id, s.techs, s.owner_user_id, f.`file`, s.company_name, s.price from spots as s join files as f on s.thumbnail_image_id = f.id where s.techs LIKE \'%' + tech + '%\';';
         const result = await db.query(sql, []);
 
         return result;
     },
-    async findByOwner (ownerId) {
+    async findByOwner(ownerId) {
 
         const result = await db.query(
-            'select s.id, f.id, s.techs, s.owner_user_id, f.`file`, s.company_name, s.price from spots as s join files as f on s.thumbnail_image_id = f.id where s.owner_user_id = ?;', 
+            'select s.id, f.id, s.techs, s.owner_user_id, f.`file`, s.company_name, s.price from spots as s join files as f on s.thumbnail_image_id = f.id where s.owner_user_id = ?;',
             [ownerId]
         );
 
         return result;
     },
-    async findById (spotId) {
+    async findById(spotId) {
         const result = await db.fetch(
-            ['id', 'techs', 'owner_user_id', 'thumbnail_image_id', 'company_name', 'price'], 
-            TABLE_NAME, {id:spotId}
-        );    
-        
+            ['id', 'techs', 'owner_user_id', 'thumbnail_image_id', 'company_name', 'price'],
+            TABLE_NAME, { id: spotId }
+        );
+
         formattedSpot = {
-            techs: result[0].techs.split(',').map(tech =>tech.trim()),
+            techs: result[0].techs.split(',').map(tech => tech.trim()),
             _id: result[0].id,
             user: result[0].owner_user_id,
             thumbnail: '1580150649451-coworkdelhi.jpg.jpg',
